cc556e1f1980e5bc3d3ef12bd6d30df9c7cde011
shariatikumail <shariati.kumail13@gmail.com>
2026-02-21 17:47:41 +0300

    feat(container="head_container ")

--- src/App.tsx
+++ src/App.tsx
@@ -8,17 +8,19 @@ const App = () => {
     <>
       <div>
          <div className="main">
-            <h1>DevHub</h1>
-            <div className="search_bar">
-              <i className="fas fa-search"></i>
-              <input type="search" id="searchBar" placeholder='Search Documentation, APIs,Tools...' />
-            </div>
-            <div className="buttons_options">
-              <button>Docs</button>
-              <button>APIs</button>
-              <button>Tools</button>
-              <button>Community</button>
-              <button className="AI_assistant_btn">AI Assistant</button>
+            <div className="head_container">
+              <h1>DevHub</h1>
+              <div className="search_bar">
+                <i className="fas fa-search"></i>
+                <input type="search" id="searchBar" placeholder='Search Documentation, APIs,Tools...' />
+              </div>
+              <div className="buttons_options">
+                <button>Docs</button>
+                <button>APIs</button>
+                <button>Tools</button>
+                <button>Community</button>
+                <button className="AI_assistant_btn">AI Assistant</button>
+              </div>
             </div>
          </div>
       </div>
