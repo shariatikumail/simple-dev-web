7bdbe5da03a2416b9e15aee711ab792956906aa0
shariatikumail <shariati.kumail13@gmail.com>
2026-02-23 15:39:56 +0300

    main.tsx i did some changes

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
-import Docs from './docs-folder/Docs.tsx';
+import Docs from './docs-folder/temp.tsx';
 
 
 createRoot(document.getElementById('root')!).render(
