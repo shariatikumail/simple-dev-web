44736d49318ffa452990054596f2f7b00d72ca28
shariatikumail <shariati.kumail13@gmail.com>
2026-03-01 16:40:27 +0300

    remove: tools

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
-import Tools from './tools/tools.tsx'
 import Login from './login/login.tsx'
 import AiAssistant from './ai-assistant/ai_assistant.tsx'
 
@@ -13,7 +12,6 @@ createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
         <Routes>
           <Route path="/" element={<App />} />
-          <Route path="/tools" element={<Tools />} />
           <Route path="/login" element={<Login />} />
           <Route path="/ai-assistant" element={<AiAssistant />} />
       </Routes>
